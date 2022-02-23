8071a44375d2a0202b7c19182ee8241a6b0766c3
kms970 <49391285+kms970@users.noreply.github.com>
2022-02-23 20:06:16 +0900

    ddd

--- mjj_mall/routes/test/testRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const controller = require('./testRouteController');
-
-router.post('/test_url',controller.testAndroidPost);
-router.get('/test_url',controller.testAndroidGet);
-
-module.exports=router;
