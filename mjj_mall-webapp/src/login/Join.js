51d51b5ec9e5a551608a75333761c92c5329f548
stsoul47 <46344256+stsoul47@users.noreply.github.com>
2022-02-25 19:19:13 +0900

    Login, Join(registry), dashboard init
    Login, Join(registry), dashboard init

---
+++ mjj_mall-webapp/src/login/Join.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+function Join() {
+
+  /**
+   * 회원가입을 요청하는 function
+   *
+   * @author jslee
+   */
+  const join = () => {
+
+  }
+
+  const vaildCheck = () => {
+
+  }
+
+  return (
+    <div>이거 동작하는건가?</div>
+  );
+}
+
+
+Join.defaultProps = {
+  memberName: '',
+  memberId: '',
+  memberPwd: '',
+  memberAddress: '',
+  memberPhoneNumber: '',
+  memberBirth: ''
+}
+
+export default Join;
