c3c10155f6ce6be6bc817527718a5ccd9153f9d4
stsoul47 <46344256+stsoul47@users.noreply.github.com>
2022-03-05 00:47:37 +0900

    고객 회원가입 구현
    고객회원가입 구현
    고객회원가입 후 redirect 구현

---
+++ mjj_mall-webapp/src/config/config.js
@@ -0,0 +1,5 @@
+export default {
+  "serverUrl": "http://58.227.231.54",
+  "serverPort": ":50001",
+  "JoinCustomer": "/user/sign-up",
+};
---
+++ mjj_mall-webapp/src/login/join/JoinComplete.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function JoinComplete() {
+  return (
+    <div>
+      <h1>회원가입 완료</h1>
+    </div>
+  )
+}
+export default JoinComplete;
