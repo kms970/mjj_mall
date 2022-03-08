8887251767ae38670651fc0e11022155462a1b64
kms970 <49391285+kms970@users.noreply.github.com>
2022-03-09 00:47:43 +0900

    test

--- mjj_mall/routes/company/companyRouteController.js
+++ mjj_mall/routes/company/companyRouteController.js
@@ -44,13 +44,13 @@ let companyRouteController = {
     },
 
     "testAndroidGet":function(req,res){
-        // mongodb.mongoSelectMany('company',{"companyName":"테스터"},{sort:{"companyIndex":-1}}).then(function(selectResult){
-        //     console.log(selectResult);
-        //     //mongodb.mongoInsertOne('company',req.body);
-        //     res.status(200).send({data:req.body});
-        // }).catch(function(err){
-        //     console.log(err);
-        // });
+        mongodb.mongoSelectMany('company',{"companyName":"테스터"},{sort:{"companyIndex":-1}}).then(function(selectResult){
+            console.log(selectResult);
+            //mongodb.mongoInsertOne('company',req.body);
+            res.status(200).send({data:req.body});
+        }).catch(function(err){
+            console.log(err);
+        });
 
         mongodb.mongoSelectAll('company').then(function(selectResult){
             console.log(selectResult);
