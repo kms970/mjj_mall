151f8cf538c4f17cc9d3c66ae3716dd3249ab3cf
kms970 <49391285+kms970@users.noreply.github.com>
2022-04-08 23:55:18 +0900

    Update todoList.js

--- mjj_mall/todoList.js
+++ mjj_mall/todoList.js
@@ -4,7 +4,7 @@
 2. Login 이후 Token validation 체크(middleware)
  2-1 Token validation 체크 할 API 하나 뚫어주기
 
-3. saveRefreshToken 수정 
+//3. saveRefreshToken 수정(완)
 4. 상품 관리 구현(DB 데이터 관리)
 5. Company ID, PWD 찾는부분 인자값 정하기
 6. 디자인 패턴 공부
