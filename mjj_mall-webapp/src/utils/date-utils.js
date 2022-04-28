24da6b1637f82bb3661906dc51b9da5d5b721059
stsoul47 <46344256+stsoul47@users.noreply.github.com>
2022-04-28 20:39:38 +0900

    Update date-utils.js
    date funtion 테스트

--- mjj_mall-webapp/src/utils/date-utils.js
+++ mjj_mall-webapp/src/utils/date-utils.js
@@ -1,3 +1,10 @@
 export function nowDate() {
   return date = new Date();
 }
+
+export function dateToFormat(date, _format) {
+  let str = '';
+  if(_format===`yyyy-mm-dd'T'HH:MM:ss`){
+    str = date.toISOString().replace(/\..*/, '');
+  }
+}
