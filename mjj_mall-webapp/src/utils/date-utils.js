ddc4011e5132259defe4b2960c0ffa180b5d8bbc
stsoul47 <46344256+stsoul47@users.noreply.github.com>
2022-04-29 23:30:23 +0900

    Update date-utils.js
    Array check, Object check function 추가

--- mjj_mall-webapp/src/utils/date-utils.js
+++ mjj_mall-webapp/src/utils/date-utils.js
@@ -8,3 +8,30 @@ export function dateToFormat(date, _format) {
     str = date.toISOString().replace(/\..*/, '');
   }
 }
+
+/**
+ * Array 여부 check, Array가 빈 값인지 check
+ * 
+ * @param {*} arr 확인할 Array를 입력 받음
+ * @returns boolean값으로 true 일경우 Array가 값이 있고, false일 경우 빈 배열
+ */
+export function isEmptyArr(arr) {
+  if(Array.isArray(arr) && arr.length === 0){
+    return true;
+  }
+  return false;
+}
+
+/**
+ * Object 여부 check, Object가 빈 값인지 check
+ * 
+ * @param {*} obj 확인할 Object를 입력 받음
+ * @returns boolean값으로 true 일 경우 Object가 값이 있고, false일 경우 빈 Object
+ */
+export function isEmptyobj(obj) {
+  if(obj.constructor === Object && Object.keys(obj).length === 0){
+    return true;
+  }
+  return false;
+}
+
