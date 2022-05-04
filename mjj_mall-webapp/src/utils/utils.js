d1d41ee24ae580bf7b32677d8a3b0ce354bfdda2
stsoul47 <46344256+stsoul47@users.noreply.github.com>
2022-05-04 23:09:28 +0900

    Update utils.js

--- mjj_mall-webapp/src/utils/utils.js
+++ mjj_mall-webapp/src/utils/utils.js
@@ -37,3 +37,7 @@ export function getRandomInt(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min);
 }
+
+export function getRandomFloat(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
